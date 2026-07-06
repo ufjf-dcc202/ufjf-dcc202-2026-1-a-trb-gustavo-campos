9ce5db1dcf7bf75353a9d9ee74c0fc5ab5f7bb6a
Gustavo Campos <gus.campos042@gmail.com>
2026-07-05 22:10:56 -0300

    modifica para que cada haste seja um elemento

--- src/main.js
+++ src/main.js
@@ -1,41 +1,55 @@
 import { TorreHanoi } from "../services/torre-hanoi.js";
 
 const QTD_DISCOS = 5;
-
-// Sequência de instruções
-
-const movimentos = [
-  { origem: "a", destino: "c" },
-  { origem: "a", destino: "b" },
-  { origem: "c", destino: "b" },
-];
+const NOME_HASTES = ["a", "b", "c"];
 
 // Simulação
 
 const torreHanoi = new TorreHanoi(QTD_DISCOS);
 const elementosVisualizacao = inicializarVisualizacao(torreHanoi);
 
-for (const instrucao of movimentos) {
-  const resultado = torreHanoi.moverDisco(instrucao.origem, instrucao.destino);
-  atualizarVisualizacao(torreHanoi, elementosVisualizacao);
-}
+atualizarVisualizacao(torreHanoi, elementosVisualizacao);
 
 // Visualização
 
 function inicializarVisualizacao(torreHanoi) {
-  const tabuleiro = document.querySelector(".tabuleiro");
-  tabuleiro.style.setProperty("--discos", torreHanoi.qtdDiscos);
+  // Obtendo elementos principais
 
-  const discos = Array.from({ length: torreHanoi.qtdDiscos }, (_, i) => {
-    const disco = document.createElement("div");
-    disco.className = "disco";
-    tabuleiro.appendChild(disco);
-    return disco;
+  const elemTabuleiro = document.querySelector(".tabuleiro");
+  elemTabuleiro.style.setProperty("--discos", torreHanoi.qtdDiscos);
+
+  const elemHastes = {
+    a: document.querySelector(".haste-a"),
+    b: document.querySelector(".haste-b"),
+    c: document.querySelector(".haste-c"),
+  };
+
+  // Criando elementos e setando valores padrões nos mesmos
+
+  NOME_HASTES.forEach((nomeHaste) => {
+    Array.from({ length: torreHanoi.qtdDiscos }, (_, i) => {
+      const elemDisco = document.createElement("div");
+
+      elemDisco.className = "disco";
+
+      const ehColunaA = nomeHaste === "a";
+      elemDisco.dataset.ativo = ehColunaA ? true : false;
+      elemDisco.dataset.tamanho = ehColunaA ? i + 1 : undefined;
+
+      elemHastes[nomeHaste].appendChild(elemDisco);
+    });
   });
 
+  const elemDiscosDaHaste = {
+    a: document.querySelectorAll(".haste-a .disco"),
+    b: document.querySelectorAll(".haste-b .disco"),
+    c: document.querySelectorAll(".haste-c .disco"),
+  };
+
   const elementosVisualizacao = {
-    tabuleiro,
-    discos,
+    elemTabuleiro,
+    elemHastes,
+    elemDiscosDaHaste,
   };
 
   atualizarVisualizacao(torreHanoi, elementosVisualizacao);
@@ -46,16 +60,25 @@ function inicializarVisualizacao(torreHanoi) {
 function atualizarVisualizacao(torreHanoi, elementosVisualizacao) {
   const dadosDiscos = torreHanoi.obterPosicoesDiscos();
 
-  if (dadosDiscos.length !== elementosVisualizacao.discos.length)
-    throw new Error("Quantidade de discos não esperada");
-
   const linhaDaOrdem = (ordem) => torreHanoi.qtdDiscos - ordem;
-  const colunaDaHaste = (haste) => ({ a: 1, b: 2, c: 3   })[haste];
+  const colunaDaHaste = (haste) => ({ a: 1, b: 2, c: 3 })[haste];
 
-  dadosDiscos.forEach((dadosDisco, index) => {
-    const elementoDisco = elementosVisualizacao.discos[index];
-    elementoDisco.style.gridRow = linhaDaOrdem(dadosDisco.ordem);
-    elementoDisco.style.gridColumn = colunaDaHaste(dadosDisco.nomeHaste);
-    elementoDisco.dataset.tamanho = dadosDisco.tamanho;
+  // Resetando
+  Object.values(elementosVisualizacao.elemDiscosDaHaste).map((elemDiscos) => {
+    elemDiscos.forEach((elemDisco) => {
+      elemDisco.dataset.ativo = false;
+      elemDisco.dataset.tamanho = null;
+    });
+  });
+
+  const ordemParaIndice = (ordem) => torreHanoi.qtdDiscos - ordem - 1;
+
+  dadosDiscos.forEach((dadosDisco) => {
+    const elemDiscos = elementosVisualizacao.elemDiscosDaHaste;
+    const indice = ordemParaIndice(dadosDisco.ordem);
+    const elemDisco = elemDiscos[dadosDisco.nomeHaste][indice];
+
+    elemDisco.dataset.ativo = true;
+    elemDisco.dataset.tamanho = dadosDisco.tamanho;
   });
 }
